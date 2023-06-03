736a57a6a3746caf4f29e00af080c097f5a2e353
Anton-Kovalevskiy <kaval1986@mail.ru>
2023-06-03 12:01:37 +0300

    Решение задачи №58

--- Program.cs
+++ Program.cs
@@ -9,6 +9,24 @@
 7 4 2 1
 9 5 3 2
 8 4 4 2
+
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Задача 58: Задайте две матрицы. 
+Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
 */
 
 Console.Write("Введите номер задачи: ");
@@ -20,6 +38,9 @@ switch (task)
     break;
     case "56":
     Task56(task);
+    break;
+    case "58":
+    Task58(task);
     break;
     default:
     break;
@@ -99,6 +120,28 @@ int MinSumRowArray2d (int [,] array2d)
     return (numberRow);
 }
 
+int [,] MultiplicationArrays (int [,] array2d1, int [,] array2d2) // Произведение двух матриц
+{
+    int [,] multArr = new int [array2d1.GetLength(0), array2d2.GetLength(1)];
+    if (array2d1.GetLength(1) != array2d2.GetLength(0)) Console.WriteLine("Произведение данных матриц не имеет смысла!");
+    else
+    {
+        for (int i = 0; i < array2d1.GetLength(0); i++)
+        {
+            for (int k = 0; k < array2d2.GetLength(1); k++)
+            {
+                for (int j = 0; j < array2d1.GetLength(1); j++)
+                {
+                        multArr [i, k] += array2d1[i, j] * array2d2[j, k];
+                }
+            }
+        }
+        Console.WriteLine("Произведение матриц:");
+        PrintIntArray2d(multArr);
+    }
+    return (multArr);
+}
+
 void Task54 (string task)                                   // Решение задачи №54
 {
     string message = "Введите количество строк массива m: ";
@@ -125,3 +168,23 @@ void Task56 (string task)                                   // Решение з
     int numRow = MinSumRowArray2d(array2d);
     Console.Write($"Минимальная сумма элемнтов в {numRow} строке");
 }
+
+void Task58(string task)                                    // Решение задачи №58
+{
+    string message = "Задайте размер m первой матрицы: ";
+    int m = size (message);
+    message = "Задайте размер n первой матрицы: ";
+    int n = size (message);
+    int [,] array2d1 = FillIntArray2d(m, n);
+    message = "Задайте размер m второй матрицы: ";
+    m = size (message);
+    message = "Задайте размер n первой матрицы: ";
+    n = size (message);
+    int [,] array2d2 = FillIntArray2d(m, n);
+    Console.WriteLine("Получена первая матрица:");
+    PrintIntArray2d (array2d1);
+    Console.WriteLine("Получена вторая матрица:");
+    PrintIntArray2d (array2d2);
+    int [,] mult = MultiplicationArrays(array2d1, array2d2);
+}
+
